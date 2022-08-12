7517f2c69fb84b1406ae752fa422e04d6637a1cb
Dmytro Stetsenko <d.a.stetsenko@gmail.com>
2022-08-12 22:07:10 +0300

    task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const body = document.querySelector('body');
+//MARKUP
+function makingGalleryMarkup(obj) {
+  return obj
+    .map(({ preview, original, description }) => {
+      return `<ul class="gallery"><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+</a></ul>`;
+    })
+    .join('');
+}
 
-console.log(galleryItems);
+const galleryMarkup = makingGalleryMarkup(galleryItems);
+gallery.innerHTML = galleryMarkup;
+
+// USAGE
+new SimpleLightbox('.gallery a', { captionDelay: 250 });
