ebc1df8312af48f088687e1c2be35f54a7c84574
Dmytro Stetsenko <d.a.stetsenko@gmail.com>
2022-08-14 00:29:11 +0300

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const TIME_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem(TIME_KEY, data['seconds']);
+}
+
+player.setCurrentTime(localStorage.getItem(TIME_KEY));
+//   .then(function (seconds) {
+//     localStorage.getItem(TIME_KEY);
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
